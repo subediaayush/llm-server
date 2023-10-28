2024912fab609412df849f471a02942513cb1b24
Aayush Subedi <subediaayush@gmail.com>
2023-10-28 16:06:18 +0100

    Remove obselete test file

--- tests/test_verification.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe } from 'node:test';
-import execute from '../src/integrations/open_clip/execute';
-
-describe('Test', () => {
-  test('verify test execution', () => {
-    expect('hello1').toBe('hello1')
-  });
-});
